1c987cb4d3c0cb29999da9f89a7785c1489cacaf
noura mamdouh <nouramamdouh25@gmail.com>
2023-06-25 13:40:22 -0700

    adding route for homepage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,6 +32,9 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
+                            <a class="nav-link icon" href="{{url('/home')}}">Home</a>
+                        </li>
+                    <li class="nav-item">
                             <a class="nav-link icon" href="#">About</a>
                         </li>
                         <li class="nav-item">
@@ -92,6 +95,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                            <a class="nav-link icon" href="{{url('/home')}}">Home</a>
+                        </li>
                     <li class="nav-item">
                             <a class="nav-link icon" href="#">About</a>
                         </li>
