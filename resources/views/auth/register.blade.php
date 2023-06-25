915eaeb5e6faa99c9d96dd4fefc4c7e25a051fad
noura mamdouh <nouramamdouh25@gmail.com>
2023-06-25 07:02:39 -0700

    creating middleware

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -105,4 +106,11 @@
         </div>
     </div>
 </div>
+@endsection
+
+
+@section('styles')
+    <style>
+        
+    </style>
 @endsection
